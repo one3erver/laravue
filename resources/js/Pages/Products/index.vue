854dd0b41192bd0efccb9f986e31796a38cedd92
FarbodHaghighi <fericom3930@gmail.com>
2024-08-10 23:27:54 +0330

    Adding routes: /products, /products/{id} , /cart/add/{id}
    Adding controllers:
    ProductController(index, show), CartController(add)

---
+++ resources/js/Pages/Products/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Product List</h1>
+        <ul>
+            <li v-for="product in products" :key="product.id">
+                {{ product.title }} - ${{ product.price }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        products: Array,
+    },
+}
+</script>
---
+++ resources/js/Pages/Products/show.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ul>
+                {{ product.title }} - ${{ product.price }}
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        product: Array,
+    },
+}
+</script>
