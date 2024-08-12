0c1616e85fb977a6b6d02786147728f3a8d0ac23
arian.abolhasanii@gmail.com <arian.abolhasanii@gmail.com>
2024-08-13 01:03:55 +0330

    improv : a layout added (MainLayout) and used for adding Header and Footer

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from "@/Components/Header.vue";
+import Footer from "@/Components/Footer.vue";
+import { AuthTypes } from "@/types";
+import { usePage } from "@inertiajs/vue3";
+
+const { auth } = usePage().props;
+</script>
+
+<template>
+    <Header :auth="auth" />
+    <slot />
+    <Footer />
+</template>
