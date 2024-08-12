4a4f5f991314b960cd69ef32f37062e7fdbb207d
Mahdi Aghamohammadi <mahdi.develoop@gmail.com>
2024-08-12 12:37:33 +0330

    feat: add admin setting CRU

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\SettingController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,5 @@ Route::prefix('admin')->middleware('admin')->group(function () {
    Route::resource('products', ProductController::class)->names('admin.products');
    Route::resource('orders', OrderController::class)->names('admin.orders')->only(['index', 'show', 'destroy']);
    Route::resource('users', UserController::class)->names('admin.users')->only(['index', 'show', 'edit', 'update']);
+   Route::resource('settings', SettingController::class)->names('admin.settings')->except(['show', 'destroy']);
 });
