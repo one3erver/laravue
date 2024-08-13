82d1bf6a8b8b7eb0a82dbef7b8ea12ba7bba5bee
arian.abolhasanii@gmail.com <arian.abolhasanii@gmail.com>
2024-08-13 15:25:04 +0330

    fix : remove default underline for links <a />

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased bg-light dark:bg-dark prose max-w-full dark:prose-invert prose-img:my-0">
+    <body class="font-sans antialiased bg-light dark:bg-dark prose max-w-full dark:prose-invert prose-img:my-0 prose-a:no-underline">
         @inertia
     </body>
 </html>
