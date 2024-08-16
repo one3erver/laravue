acf949ff4939e40bcd6f74cd43f4863c4ed19359
arian.abolhasanii@gmail.com <arian.abolhasanii@gmail.com>
2024-08-16 12:50:18 +0330

    Add : Post cart to the route 'carts.store'

--- resources/js/util/useCart.ts
+++ resources/js/util/useCart.ts
@@ -1,4 +1,6 @@
-export function useAddToLocalCart(product_id: number, quantity: number) {
+import { router } from "@inertiajs/vue3";
+
+export function useAddToLocalCart(id: number, count: number) {
     //check if we have a cart in localstorage, if we do add to that cart
     if (localStorage.getItem("cart")) {
         //turn our cart to a array
@@ -8,7 +10,7 @@ export function useAddToLocalCart(product_id: number, quantity: number) {
 
         //check if we have the product we are trying to add in our cart, if we dont the returned value would be -1
         let indexOfThisProductInCart = cart.findLastIndex((item) => {
-            if (item.product_id === product_id) {
+            if (item.product_id === id) {
                 return true;
             }
         });
@@ -16,15 +18,15 @@ export function useAddToLocalCart(product_id: number, quantity: number) {
         //if we have this product in our cart, update the quantity
         if (indexOfThisProductInCart != -1) {
             //if quantity reaches 0 delete it from the cart, else update it
-            if (quantity <= 0) {
+            if (count <= 0) {
                 cart.splice(indexOfThisProductInCart, 1);
             } else {
-                cart[indexOfThisProductInCart].quantity = quantity;
+                cart[indexOfThisProductInCart].quantity = count;
             }
         }
         //if we dont have this product in our cart, add it in
         else {
-            cart.push({ product_id, quantity });
+            cart.push({ id, quantity: count });
         }
 
         //dispatch cartchange event if a item is added
@@ -39,11 +41,14 @@ export function useAddToLocalCart(product_id: number, quantity: number) {
 
         //atlast store the cart as a string in localstorage
         localStorage.setItem("cart", JSON.stringify(cart));
+
+        //post data
+        router.post(route("carts.store"), { cart_list: JSON.stringify(cart) });
     }
     //if we dont have a cart, create an empty one and then run this again
     else {
         localStorage.setItem("cart", JSON.stringify([]));
-        useAddToLocalCart(product_id, quantity);
+        useAddToLocalCart(id, count);
     }
 }
 
