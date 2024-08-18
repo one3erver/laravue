05882e6b4158a4132c134bd807876a538e355f7a
FarbodHaghighi <fericom3930@gmail.com>
2024-08-18 11:50:14 +0330

    Edit : OrderController, ProductController (sending each product's count in the user's cart to Landing page)

--- app/Http/Controllers/Customer/ProductController.php
+++ app/Http/Controllers/Customer/ProductController.php
@@ -4,19 +4,45 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class ProductController extends Controller
 {
     //For showing all products in the page
     public function index(){
+        $user = Auth::user();
         $products = Product::all();
+
+        if ($user){
+            $userCarts = $user->carts;
+            foreach ($products as $product) {
+                foreach ($userCarts as $cart) {
+                    if ($cart->product_id == $product->id) {
+                        $product->count = $cart->count;
+                    }else{
+                        $product->count = 0;
+                    }
+                }
+            }
+        }
+        else{
+            foreach ($products as $product) {
+                $product->count = -1;
+            }
+        }
         return Inertia::render('Landing', compact('products'));
     }
 
     //For Showing a product with a specific ID
     public function show($id){
+        $user = Auth::user();
         $product = Product::findOrFail($id);
-        return Inertia::render('Product', compact('product'));
+        $product_Count_In_Cart = $user->carts()->where('product_id', $product->id)->first()->count;
+        $submittedContent = [
+            'product_count' => $product_Count_In_Cart,
+            'product' => $product,
+        ];
+        return Inertia::render('Product', compact('submittedContent'));
     }
 }
