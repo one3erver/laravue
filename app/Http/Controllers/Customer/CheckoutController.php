de66a918bcc2d60d8231ae13aed78f75f41d9ec3
FarbodHaghighi <fericom3930@gmail.com>
2024-08-24 18:22:18 +0330

    Edit : CheckoutController

--- app/Http/Controllers/Customer/CheckoutController.php
+++ app/Http/Controllers/Customer/CheckoutController.php
@@ -24,7 +24,7 @@ class CheckoutController extends Controller
         if (session('redirect')){
             session()->forget('redirect');
             $submittedContent['wallet_id'] = $invoice->wallet_id;
-            return view('test_1', compact('submittedContent'));
+//            return view('test_1', compact('submittedContent'));
             return Inertia::render('Checkout', $submittedContent);
         }
         else{
@@ -33,7 +33,7 @@ class CheckoutController extends Controller
             $wallet_id = $wallets[$invoice->wallet_id];
             $submittedContent['wallet_id'] = $wallet_id;
 
-            return view('test_1', compact('submittedContent'));
+//            return view('test_1', compact('submittedContent'));
             return Inertia::render('Checkout', $submittedContent);
         }
     }
@@ -43,6 +43,7 @@ class CheckoutController extends Controller
         $invoice = Invoice::find($request->input('invoice_id'));
         $transaction_id = $request->input('transaction_id');
 
+//      Check if entered transaction_id has already been entered by someone else, it will give an error message
         $existingTransaction = Invoice::where('transaction_id', $transaction_id)->first();
         if ($existingTransaction) {
             session(['redirect' => true]);
@@ -51,36 +52,37 @@ class CheckoutController extends Controller
                 'hint' => 'If you think there is a problem, contact us by email.'
             ]);
         }
-
-        $response = json_decode(Http::get('https://apilist.tronscan.org/api/new/token_trc20/transfers', [
-            'direction' => 'in',
-            'count' => 5,
-            'tokens' => 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t',
-            'relatedAddress' => 'TCN6S5TfAPYf6aWbSgotEfsmNWgoDwa1Gm'
-        ]));
+        else{
+            $response = json_decode(Http::get('https://apilist.tronscan.org/api/new/token_trc20/transfers', [
+                'direction' => 'in',
+                'count' => 5,
+                'tokens' => 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t',
+                'relatedAddress' => 'TCN6S5TfAPYf6aWbSgotEfsmNWgoDwa1Gm'
+            ]));
 
 //      Search in the api to match the TransactionID and Transferred Tether with transactions
-        $totalCost = $invoice->order->total_cost;
-        foreach ($response->token_transfers as $transaction) {
-            if ($transaction->transaction_id == $transaction_id) {
-                if (($transaction->quant/1000000) >= $totalCost){
+            $totalCost = $invoice->order->total_cost;
+            foreach ($response->token_transfers as $transaction) {
+                if ($transaction->transaction_id == $transaction_id) {
+                    if (($transaction->quant/1000000) >= $totalCost){
 //                  If validation is correct, add TrackingCode and TransactionID to DB
-                    $invoiceController = new InvoiceController();
-                    $invoiceController->update($invoice, $transaction_id);
+                        $invoiceController = new InvoiceController();
+                        $invoiceController->update($invoice, $transaction_id);
 //                  Set the success session for middleware then redirect the User to a successful page
-                    session(['payment_successful' => true]);
-                    return to_route('checkouts.success');
+                        session(['payment_successful' => true]);
+                        return to_route('checkouts.success');
+                    }
+                    else{
+                        session(['redirect' => true]);
+                        return to_route('checkouts.show')->with([
+                            'error' => 'Tether transferred is lower than your order price. Please pay the order amount in full.',
+                            'hint' => 'To track the Tether amount already transferred, contact us via email.'
+                        ]);
+                    }
                 }
                 else{
-                    session(['redirect' => true]);
-                    return to_route('checkouts.show')->with([
-                        'error' => 'Tether transferred is lower than your order price. Please pay the order amount in full.',
-                        'hint' => 'To track the Tether amount already transferred, contact us via email.'
-                    ]);
-                }
-            }
-            else{
 
+                }
             }
         }
     }
