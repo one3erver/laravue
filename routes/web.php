2ef952cdc75e3c944e9a82b8a8f63e81e651d1d3
FarbodHaghighi <fericom3930@gmail.com>
2024-08-26 13:55:13 +0330

    Edit: CheckoutController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Customer\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
